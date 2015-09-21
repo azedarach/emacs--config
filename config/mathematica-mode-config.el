098dd752c8859de539fdb7e6ff9ebb43a89aea58
Dylan Harries <dylan.harries@adelaide.edu.au>
2015-09-21 10:58:22 +0930

    Add trivial mathematica-mode config

---
+++ config/mathematica-mode-config.el
@@ -0,0 +1,7 @@
+;;;; Configuration options for mathematica-mode
+;;   Loaded by init.el
+
+;;;; Options for default mathematica-mode
+;; ========================================
+;;; No indentation
+(add-to-list 'auto-mode-alist '("\\.m\\'" . text-mode))
