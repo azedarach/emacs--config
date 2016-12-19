a4f96105b00e4fba8286ee91cf2244f1f637812a
Dylan Harries <dylan.harries@adelaide.edu.au>
2016-12-20 10:04:54 +1030

    Replace tabs with spaces when editing Mathematica source code

--- config/mathematica-mode-config.el
+++ config/mathematica-mode-config.el
@@ -4,4 +4,11 @@
 ;;;; Options for default mathematica-mode
 ;; ========================================
 ;;; No indentation
+(defun mathematica-mode-hook ()
+  (if (string-suffix-p ".m" buffer-file-name)
+      (setq tab-width 8
+	    indent-tabs-mode nil)))
+
+(add-hook 'text-mode-hook 'mathematica-mode-hook)
+
 (add-to-list 'auto-mode-alist '("\\.m\\'" . text-mode))
